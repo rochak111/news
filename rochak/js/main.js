3694a1c65d69e649aeb35a5aee74f518274db790
Rochak <rochakbhattarai11@gmail.com>
2022-04-13 03:23:42 +0530

    Times of Cgu

---
+++ rochak/js/main.js
@@ -0,0 +1,11 @@
+//Search bar JavaScript
+
+// FIlter anything
+$(document).ready(function () {
+  $("#anythingSearch").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#myDIV *").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+});
